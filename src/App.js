96486b2da06a0d2e6642f764278c623ed066ab0e
Vitaliy <43200383+s04v@users.noreply.github.com>
2022-03-11 12:12:12 +0100

    test

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-          Hello world
+          hello world
       </header>
     </div>
   );
